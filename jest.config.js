5c1b1d81237651a6c042ce2cbdcf9f7d05725554
John Raymond <john.raymond.blando@gmail.com>
2024-06-11 10:27:21 +0800

    Initial setup test for report routes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'jsdom'
+    // preset: 'ts-jest/presets/js-with-ts',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ["**/**/*.spec.ts"],
+    verbose: true,
+    forceExit: true,
+    // clearMocks: true,
 };
